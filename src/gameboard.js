ca0d5e0cbb2e12677d9402aef1b2dd44171fdcd5
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-08 16:54:38 -0400

    add f place ship

--- src/gameboard.js
+++ src/gameboard.js
@@ -7,5 +7,27 @@ export class Gameboard {
   missed = [];
 
 
-  
+  placeShip(length, letter, number, direction = 'horizontal'){
+    const ship = new Ship(length);
+    const shipsPosition = [];
+
+    for (let i = 0; i < length; i++) {
+        const positionLetter = direction == 'horizontal' ? letter : letter + i;
+        const positionNumber = direction == 'horizontal' ? number + i : number;
+  }
+    if (positionNumber < 0 || positionNumber > 9 || positionLetter < 0 || positionLetter > 9) {
+        throw new Error("ship out of board limit")
+    }
+    if (this.board[positionLetter][positionNumber] !== null) {
+        throw new Error("Ships overlap");
+      }
+
+      shipsPosition.push([positionLetter, positionNumber]);
+}
+    shipsPosition.forEach(([positionLetter, positionNumber]) => {
+        this.board[positionLetter, positionNumber] = ship; 
+        
+    });
+    this.ships.push(ship);
+
 }
